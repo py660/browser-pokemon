60b63339d0027741637aed0d0c416cda08f0ed8b
py660 <93660666+py660@users.noreply.github.com>
2024-01-26 10:53:59 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-alert("Hi!!!")
-
+let img = document.createElement("img")
+img.src = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
+img.style.position = "fixed";
+img.style.left = "50px";
+img.style.top = "50px";
+document.body.append(img);
 //javascript: (function(){var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://py660.github.io/browser-pokemon/index.js?cache='+(new Date()).toString());document.body.appendChild(jsCode);var listener = document.addEventListener("securitypolicyviolation", (e) => {if (e.blockedURI.startsWith("https://py660.github.io/browser-pokemon/index.js")){alert("Script blocked by CSP. Please try again on another website.");document.removeEventListener("securitypolicyviolation", listener);}});}())
