38ceac877d56472386129d6e3e047043f5f8e60b
py660 <93660666+py660@users.noreply.github.com>
2024-01-29 10:40:22 -0500

    add error catching

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+try{
 let img = document.createElement("img")
 img.src = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
 img.style.position = "fixed";//clientWidth, clientHeight
@@ -10,3 +11,6 @@ img.style.top = Math.floor(Math.random()*(window.clientHeight-height));
 img.style.zIndex = "999999999999";
 document.body.append(img);
 //javascript: (function(){var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://py660.github.io/browser-pokemon/index.js?cache='+(new Date()).toString());document.body.appendChild(jsCode);var listener = document.addEventListener("securitypolicyviolation", (e) => {if (e.blockedURI.startsWith("https://py660.github.io/browser-pokemon/index.js")){alert("Script blocked by CSP. Please try again on another website.");document.removeEventListener("securitypolicyviolation", listener);}});}())
+}catch(e){
+    alert(e);
+}
