8b6356005da4ac6554afaa5a377bfbddff2790cd
py660 <93660666+py660@users.noreply.github.com>
2024-01-31 21:00:58 -0500

    Fix bugs

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ try{
 let img = document.createElement("img")
 img.src = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
 img.style.position = "fixed";//clientWidth, clientHeight
-let width  = "50px";
-let height = "50px";
-img.width = width;
-img.height = height;
-img.style.left = Math.floor(Math.random()*(window.clientWidth-width));
-img.style.top = Math.floor(Math.random()*(window.clientHeight-height));
+let width  = 50;
+let height = 50;
+img.style.width = width + "px";
+img.style.height = height + "px";
+img.style.left = Math.floor(Math.random()*(window.innerWidth-width)) + "px";
+img.style.top = Math.floor(Math.random()*(window.innerHeight-height)) + "px";
 img.style.zIndex = "999999999999";
 document.body.append(img);
 //javascript: (function(){var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://py660.github.io/browser-pokemon/index.js?cache='+(new Date()).toString());document.body.appendChild(jsCode);var listener = document.addEventListener("securitypolicyviolation", (e) => {if (e.blockedURI.startsWith("https://py660.github.io/browser-pokemon/index.js")){alert("Script blocked by CSP. Please try again on another website.");document.removeEventListener("securitypolicyviolation", listener);}});}())
