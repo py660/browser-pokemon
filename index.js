6b974dde58bf1edd4d273ab4b7a7b4e8aeac60b0
py660 <93660666+py660@users.noreply.github.com>
2024-02-03 18:03:33 -0500

    commit

--- index.js
+++ index.js
@@ -28,21 +28,5 @@ function doBrowserPokemon(){
     }
 }
 /*
-javascript: (function(){
-    if (typeof doBrowserPokemon == "function"){
-        alert("hi")
-        doBrowserPokemon();
-    }
-    else{
-        var jsCode = document.createElement('script');
-        jsCode.setAttribute('src', 'https://py660.github.io/browser-pokemon/index.js?cache='+(new Date()).toString());
-        document.body.appendChild(jsCode);
-        var listener = document.addEventListener("securitypolicyviolation", (e) => {
-            if (e.blockedURI.startsWith("https://py660.github.io/browser-pokemon/index.js")){
-                alert("Script blocked by CSP. Please try again on another website.");
-                document.removeEventListener("securitypolicyviolation", listener);
-            }
-        });
-    }
-}())
+javascript: (function(){if (typeof doBrowserPokemon == "function"){alert("hi");doBrowserPokemon();}else{var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://py660.github.io/browser-pokemon/index.js?cache='+(new Date()).toString());document.body.appendChild(jsCode);var listener = document.addEventListener("securitypolicyviolation", (e) => {if (e.blockedURI.startsWith("https://py660.github.io/browser-pokemon/index.js")){alert("Script blocked by CSP. Please try again on another website.");document.removeEventListener("securitypolicyviolation", listener);}});jsCode.onload = ()=>{alert("Running browser pokemon...");doBrowserPokemon();}}}())
 */
