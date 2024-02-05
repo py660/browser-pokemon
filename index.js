b601bbd8bf40915ce6e0945c8495a5df4e257e85
py660 <93660666+py660@users.noreply.github.com>
2024-02-05 15:44:05 -0500

    fix

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ setInterval(()=>{
             e.x += e.dy;
             img.style.left = e.x + "px";
             img.style.top = e.y + "px";
-            console.log(e.dx, e.dy);
+            console.log(img.x, window.innerWidth-e.width);
             if (img.x >= window.innerWidth-e.width || img.x <= 0) e.dx *= -1; e.x = window.innerWidth-e.width;
             if (img.y >= window.innerHeight-e.height || img.y <= 0) e.dy *= -1; e.y = window.innerHeight-e.height;
         });
