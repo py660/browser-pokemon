145bc6d6f0863ac69d98448e5caf70cf5707a27e
py660 <93660666+py660@users.noreply.github.com>
2024-02-06 14:30:41 -0500

    remove comments add console log

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Make the DIV element draggable:
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  // if present, the header is where you move the DIV from:
   elmnt.onmousedown = dragMouseDown;
   let id = elmnt.id;
 
@@ -9,33 +7,29 @@ function dragElement(elmnt) {
     browserPokemon[id].dragging = true;
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     browserPokemon[id].x = elmnt.offsetTop - pos2;
     browserPokemon[id].y = elmnt.offsetLeft - pos1;
+    console.log(browserPokemon[id].x, browserPokemon[id].y);
   }
 
   function closeDragElement() {
 
     browserPokemon[id].dragging = false;
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
