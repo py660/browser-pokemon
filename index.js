f79396c9d44e833f0b1379ab91b833b8695116b8
py660 <93660666+py660@users.noreply.github.com>
2024-02-14 14:04:56 -0500

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+let pokemon = [
+    "blaziken.gif",
+    "charizard constant.gif",
+    "charizard idle.gif",
+    "charizard usually.gif",
+    "cyndaquil.gif",
+    "darkrai.gif",
+    "dawnwings.gif",
+    "ditto.gif",
+    "eevee.gif",
+    "galarianmoltres.gif",
+    "genesect.gif",
+    "gengar.gif",
+    "greninja butnormal.gif",
+    "greninja.gif",
+    "groudon.gif",
+    "hisuiandecidueye.gif",
+    "infernape.gif",
+    "klefki.gif",
+    "kyogre.gif",
+    "kyuremblack.gif",
+    "lucario.gif",
+    "lugia glitchy.gif",
+    "lugia.gif",
+    "megablaziken.gif",
+    "mewtwo.gif",
+    "mimikyu.gif",
+    "obstagoon.gif",
+    "pikachu idle.gif",
+    "pikachu notalpha.gif",
+    "pikachu running.gif",
+    "pnytagalar.gif",
+    "pooper.gif",
+    "raichu.gif",
+    "rayquaza.gif",
+    "reshiram.gif",
+    "rockruff.gif",
+    "sceptile.gif",
+    "scorbunny.gif",
+    "shinyyveltal.gif",
+    "suicune.gif",
+    "tapukoko.gif",
+    "tinkaton.gif",
+    "torterra.gif",
+    "vaporeon.gif",
+    "wooper.gif"
+]
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   elmnt.onmousedown = dragMouseDown;
@@ -37,34 +85,37 @@ function dragElement(elmnt) {
 
 let browserPokemon = {};
 function doBrowserPokemon(){
-    try{
-        function uuidv4() {
-            return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-            );
+    for (let image of pokemon){
+        try{
+            function uuidv4() {
+                return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+                    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+                );
+            }
+            let img = document.createElement("img");
+            img.src = "https://py660.github.io/browser-pokemon/pokemon/"+image;
+            img.style.all = "unset";
+            img.style.position = "fixed";
+            img.style.cursor = "grab";
+            img.onload = ()=>{
+                let width  = img.width;
+                let height = img.height;
+                let x = Math.floor(Math.random()*(window.innerWidth-width));
+                let y = Math.floor(Math.random()*(window.innerHeight-height));
+                let uuid = uuidv4();
+                img.id = uuid;
+                browserPokemon[uuid] = {dragging: false, id: uuid, width: width, height: height, x: x, y: y, dx: Math.random()*2-1, dy: Math.random()*2-1};
+                img.style.width = width + "px";
+                img.style.height = height + "px";
+                img.style.left = x + "px";
+                img.style.top = y + "px";
+                img.style.zIndex = "999999999999";
+                document.body.append(img);
+                dragElement(img);
+            }
+        }catch(e){
+            alert(e);
         }
-        let img = document.createElement("img");
-        img.src = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024";
-        img.style.all = "unset";
-        img.style.position = "fixed";
-        img.style.cursor = "grab";
-        let width  = 50;
-        let height = 50;
-        let x = Math.floor(Math.random()*(window.innerWidth-width));
-        let y = Math.floor(Math.random()*(window.innerHeight-height));
-        let uuid = uuidv4();
-        img.id = uuid;
-        browserPokemon[uuid] = {dragging: false, id: uuid, width: width, height: height, x: x, y: y, dx: Math.random()*2-1, dy: Math.random()*2-1};
-        img.style.width = width + "px";
-        img.style.height = height + "px";
-        img.style.left = x + "px";
-        img.style.top = y + "px";
-        img.style.zIndex = "999999999999";
-        document.body.append(img);
-        dragElement(img);
-        window.browserPokemon = browserPokemon;
-    }catch(e){
-        alert(e);
     }
 }
 setInterval(()=>{
